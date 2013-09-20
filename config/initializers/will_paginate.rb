6edc903bcd77c346bdb4d0cc85c29c3b1cb2ec81
liuhuyu <735077491@qq.com>
2013-09-20 20:35:14 +0800

    bootstrap 3.0.0 support for will_paginate

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -11,7 +11,7 @@ if defined?(WillPaginate)
         protected
 
         def html_container(html)
-          tag :div, tag(:ul, html), container_attributes
+          tag :div, tag(:ul, html, :class => "pagination"), container_attributes
         end
 
         def page_number(page)
